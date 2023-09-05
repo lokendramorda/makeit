0c69ed9d1d2a6907dc15a1a0648badc89f775360
lokendra morda <lokendramorda@gmail.com>
2023-09-05 17:34:07 +0530

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
+      'x-rapidapi-key': "140a89deddmsh056ea9cae164d63p1ea2a8jsne479cb6f3563",
     },
   });
     
